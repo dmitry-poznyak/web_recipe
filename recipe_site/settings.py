5e81c3cd5f869d74b0f14c32101416bec68b1a5c
dmitry-poznyak <dimapoznyak1227@gmail.com>
2025-05-16 23:07:33 +0300

    fix

--- recipe_site/settings.py
+++ recipe_site/settings.py
@@ -7,7 +7,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY')
 DEBUG = os.environ.get('DEBUG', 'True') == 'True'
 
-ALLOWED_HOSTS = ['.onrender.com', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['web-recipe.onrender.com', 'localhost', '127.0.0.1']
 
 INSTALLED_APPS = [
     'django.contrib.admin',
