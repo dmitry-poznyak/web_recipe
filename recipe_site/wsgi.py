a0abaf0a494c32625a85153d0e223f6c5e72adb7
dmitry-poznyak <dimapoznyak1227@gmail.com>
2025-05-16 22:19:48 +0300

    help gpt

--- recipe_site/wsgi.py
+++ recipe_site/wsgi.py
@@ -1,16 +1,12 @@
-"""
-WSGI config for recipe_site project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/5.2/howto/deployment/wsgi/
-"""
-
 import os
-
+import django
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'recipe_site.settings')
+django.setup()
+
+
+from django.core.management import call_command
+call_command('migrate', interactive=False)
 
 application = get_wsgi_application()
