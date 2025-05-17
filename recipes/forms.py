4af38fa1bda88800531c02805a491c1c1fbc104e
dmitry-poznyak <dimapoznyak1227@gmail.com>
2025-05-17 21:40:01 +0300

    изменил формы

--- recipes/forms.py
+++ recipes/forms.py
@@ -10,8 +10,14 @@ class RecipeForm(forms.ModelForm):
 
 
 class SignUpForm(UserCreationForm):
-    email = forms.EmailField(required=True)
+    email = forms.EmailField(required=True, widget=forms.EmailInput(attrs={'class': 'form-control'}))
 
     class Meta:
         model = User
         fields = ('username', 'email', 'password1', 'password2')
+        widgets = {
+            'username': forms.TextInput(attrs={'class': 'form-control'}),
+            'password1': forms.PasswordInput(attrs={'class': 'form-control'}),
+            'password2': forms.PasswordInput(attrs={'class': 'form-control'}),
+        }
+
