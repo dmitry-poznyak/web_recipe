4938b8aee30e1fdce9e5302a91995ad50be27cb3
dmitry-poznyak <dimapoznyak1227@gmail.com>
2025-05-17 23:10:39 +0300

    fix

--- recipe_site/settings.py
+++ recipe_site/settings.py
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'recipe_site.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'recipes' / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
